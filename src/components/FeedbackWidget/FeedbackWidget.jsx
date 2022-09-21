ef91231decf472fa70a10275968361612ea07885
Stanislav2022 <tsv5@ukr.net>
2022-09-21 19:03:14 +0300

    1

--- src/components/FeedbackWidget/FeedbackWidget.jsx
+++ src/components/FeedbackWidget/FeedbackWidget.jsx
@@ -1,5 +1,32 @@
 import { Component } from "react";
 
 export default class FeedbackWidget extends Component {
-    
+    state = {
+     good: 0,
+     neutral: 0,
+     bad: 0
+    }
+    render() {
+        const { good, neutral, bad } = this.state;
+        return (
+            <div>
+                <div>
+                    <h2>Please leave feedback</h2>
+                    <button>Good</button>
+                    <button>Neutral</button>
+                    <button type="submit">Bad</button>
+                </div>
+                <div>
+                    <h2>Statistics</h2>
+                    <ul>
+                        <li>Good: {good}</li>
+                        <li>Neutral: {neutral}</li>
+                        <li>Bad: {bad}</li>
+                        {/* <li>Total:</li>
+                        <li>Positive feedback:</li> */}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
 }
